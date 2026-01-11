fe5e5ae5e790693022d2b5313282199fa4e71b88
uzeni-stack <frantisekhonal@gmail.com>
2026-01-11 11:30:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBUFpm-YlzJX_wd0hoe9kc0gXdteflLGBQ",
+  apiKey: "TVOJE_API_KEY",
   authDomain: "e-shop-685c2.firebaseapp.com",
+  databaseURL: "https://e-shop-685c2-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "e-shop-685c2",
-  storageBucket: "e-shop-685c2.firebasestorage.app",
+  storageBucket: "e-shop-685c2.appspot.com",
   messagingSenderId: "923573029060",
-  appId: "1:923573029060:web:152c25dba0bc272e562191",
-  measurementId: "G-Z81TJBVTS1"
+  appId: "1:923573029060:web:152c25dba0bc272e562191"
 };
-firebase.initializeApp(firebaseConfig);
+
+// Inicializace Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 const db = firebase.database();
-const storage = firebase.storage();
