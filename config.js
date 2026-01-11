abba597c1f2eeb874943f09ec2847848e7234eb1
uzeni-stack <frantisekhonal@gmail.com>
2026-01-11 10:50:55 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUFpm-YlzJX_wd0hoe9kc0gXdteflLGBQ",
+  authDomain: "e-shop-685c2.firebaseapp.com",
+  projectId: "e-shop-685c2",
+  storageBucket: "e-shop-685c2.firebasestorage.app",
+  messagingSenderId: "923573029060",
+  appId: "1:923573029060:web:152c25dba0bc272e562191",
+  measurementId: "G-Z81TJBVTS1"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const storage = firebase.storage();
