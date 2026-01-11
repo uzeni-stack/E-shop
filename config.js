9b2c27ce54f69c2a8b778f84bb383a4e8b130bdb
uzeni-stack <frantisekhonal@gmail.com>
2026-01-11 17:25:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 // --- KONFIGURACE FIREBASE ---
-// Tady jsou tvé skutečné údaje pro připojení k databázi
 const firebaseConfig = {
   apiKey: "AIzaSyByN-8y4_fM-W_m_m1Z2X3y4z5a6b7c8d9e", 
   authDomain: "uzeni-stack.firebaseapp.com",
-  databaseURL: "https://uzeni-stack-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://uzeni-stack-default-rtdb.europe-west1.firebasedatabase.app", // OPRAVENO
   projectId: "uzeni-stack",
   storageBucket: "uzeni-stack.appspot.com",
   messagingSenderId: "587023363385",
@@ -16,8 +15,6 @@ if (!firebase.apps.length) {
 }
 const db = firebase.database();
 
-// --- ZÁLOŽNÍ KLÍČ PRO GITHUB (Rozdělený, aby ho robot nenašel) ---
-const cast_1 = 'ghp_kIgSOJKu';
-const cast_2 = '0DeEqFN926no4tr0TAZUu837wmmd';
-
-const GITHUB_ZALOZNI = cast_1 + cast_2;
+// --- POMOCNÉ PROMĚNNÉ ---
+const cast_1 = 'ghp_'; // Start tokenu
+const cast_2 = '';      // Tady to necháme prázdné, token budeme brát z databáze
